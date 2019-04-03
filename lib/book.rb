1e83d4ef9e0e9e020010751883dec85bad2f8b1d
cogilvy <chrisogilvy@cox.net>
2019-04-03 19:06:03 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  # This is a class constant, which is shared among all instances.
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +13,38 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+ # This will add the different genres to the empty array to keep track of
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
 end
+
+
+# name spacing
+# When you see Book::GENRES in an error message, this is called name spacing!
+# This means that the thing on the right side of the colons is a thing defined
+# within the context of the thing on the left side of the colons.
+# Therefore there is a GENRE constant inside the Book class!
+
+
+# If we look in rspec you will see:
+# describe 'GENRES' do
+#   it 'keeps track of all genres' do
+#     genres = ["Thriller", "Science Fiction", "Romance"]
+#     genres.each_with_index do |genre, i|
+#       book = Book.new("Book_#{i}")
+#       book.genre = genre
+#     end
+#
+#     genres.each do |genre|
+#       expect(Book::GENRES).to include(genre)
+#     end
+#   end
+# end
+
+# This means....
+# The test is creating a bunch of books.
+# The test is assigning each of those books a genre.
+# The test is expecting our GENRES class constant to keep track of those genres
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
+  # When we initialize a new Shoe, it will be given a brand. We want to add that
+  # brand to the BRANDS array, then do BRANDS.uniq! to eliminate any repeats.
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
