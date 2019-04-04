c5d395ae660907ab0217c2bd881b969eca34f243
github username <github email address>
2019-04-04 19:43:04 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,15 @@
+require 'pry'
 class Shoe
+
+  #attr_accessor is our getter/setter creators currently we are creating color, size , material and condition
+  #attr_reader contains our brand which only allows us to read our brand
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+#BRANDS is our constant class, we know because it is in ALL caps. It will contain the brands
   BRANDS =[]
 
+# Initialize is invoked with an instance is created. In this case when our brand is being created we are then adding it to our BRANDS array. Also we have our .uniq! method that is creating a new array with only uniq items or in our case brands. 
   def initialize(brand)
     @brand = brand
     BRANDS << brand
@@ -14,5 +20,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-end 
+end
